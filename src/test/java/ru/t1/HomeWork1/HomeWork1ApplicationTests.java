321b44c218ca38af1d9393d073fd40ae6f50767f
Artem Privalov <rashen95@mail.ru>
2024-06-30 01:18:17 +0300

    initial commmit

---
+++ src/test/java/ru/t1/HomeWork1/HomeWork1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1.HomeWork1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
